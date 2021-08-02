71170271fc43fb4e5eef2b88164469cd851296af
KJTang <kaijietang@outlook.com>
2021-08-02 21:42:09 +0800

    feature: load semantic model & do diagnostic

---
+++ Assets/Scripts/CSharpHotfix/Evaluator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace CSharpHofix
+{
+
+    public class Evaluator
+    {
+
+        public void Evaluate(SyntaxNode node)
+        {
+            //
+        }
+    }
+    
+}
--- Assets/Scripts/RoslynTest.cs
+++ Assets/Scripts/RoslynTest.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -34,8 +37,6 @@ public class RoslynTest : MonoBehaviour
     void LoadAllFiles()
     {
         var dirPath = GetHotfixDirPath();
-        Debug.Log("#ROSLYN# Hotfix Path: " + dirPath);
-
         var dirInfo = new DirectoryInfo(dirPath);
         var files = dirInfo.GetFiles("*.cs", SearchOption.AllDirectories);
         for (var i = 0; i != files.Length; ++i)
@@ -45,23 +46,69 @@ public class RoslynTest : MonoBehaviour
             using (var streamReader = fileInfo.OpenText())
             {
                 var programText = streamReader.ReadToEnd();
-                Func(programText);
+                Func(fileInfo.FullName, programText);
             }
         }
     }
 
-    void Func(string programText)
+    IEnumerable<Assembly> GetAllAssemblies()
     {
+        var app = AppDomain.CurrentDomain;
+        var allAssemblies = app.GetAssemblies();
+        return allAssemblies;
+    }
+
+    IEnumerable<Assembly> GetReferencableAssemblies()
+    {
+        return GetAllAssemblies().Where(assembly => !assembly.IsDynamic && !string.IsNullOrEmpty(assembly.Location));
+    }
+
+    void Func(string filePath, string programText)
+    {
+        // syntax
         SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+        //CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
         Debug.Log("#ROSLYN# program: \n" + tree.ToString());
 
 
-        //Debug.Log($"The tree is a {root.Kind()} node.");
-        //Debug.Log($"The tree has {root.Members.Count} elements in it.");
-        //Debug.Log($"The tree has {root.Usings.Count} using statements. They are:");
-        //foreach (UsingDirectiveSyntax element in root.Usings)
-        //    Debug.Log($"\t{element.Name}");
+        // semantic
+        var references = new List<MetadataReference>();
+        var assemblies = GetReferencableAssemblies();
+        foreach (var assembly in assemblies)
+        {
+            //Debug.Log("#ROSLYN# assemblies: " + assembly);
+            references.Add(MetadataReference.CreateFromFile(assembly.Location));
+        }
+        var compilation = CSharpCompilation.Create("HelloWorld")
+            .AddReferences(references)
+            .AddSyntaxTrees(tree);
+        
 
-        CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+        // diagnostic
+        SemanticModel model = compilation.GetSemanticModel(tree);
+        var diagnostics = model.GetDiagnostics();
+        var hasError = false;
+        for (var i = 0; i != diagnostics.Length; ++i)
+        {
+            var diagnostic = diagnostics[i];
+            var log = "#ROSLYN# diagnostics: level: " + diagnostic.WarningLevel + " \t" + diagnostic;
+            if (diagnostic.WarningLevel == 0)
+            {
+                hasError = true;
+                Debug.LogError(log);
+            }
+            else
+            {
+                Debug.LogWarning(log);
+            }
+        }
+        if (hasError)
+        {
+            Debug.LogError("#ROSLYN# error occured when load file: " + filePath);
+            return;
+        }
+
+
+        // TODO: evaluate
     }
 }
